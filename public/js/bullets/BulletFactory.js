6bdb93b09e7b237b68ca433c24ec2331d1552a66
prescottbreeden <prescott.breeden@gmail.com>
2018-09-21 17:08:39 -0700

    spread fire is now making individual bullets to the front

--- public/js/bullets/BulletFactory.js
+++ public/js/bullets/BulletFactory.js
@@ -4,17 +4,36 @@ class BulletFactory {
 		this.canvas = game.canvas;
 		this.context = game.context;
 		this.ship = ship;
-
 		this.bullets = [];
 
+		// BLASTER
 		this.blasterShot = () => {
-			const bullet = new Blaster(this.game, this.ship);
+			let bullet = new Blaster(this.game, this.ship);
 			this.bullets.push(bullet);
 		}
 		
+		// SPREAD SHOT
 		this.spreadShot = () => {
-			const bullet = new Spread(this.game, this.ship);
+			const bullet = new Spread(this.game, this.ship, this.ship.x - 2.5);
 			this.bullets.push(bullet);
+			if(this.ship.weaponStr > 1) {
+				let spacing = this.ship.w/2;
+				const bullet3 = new Spread(this.game, this.ship, this.ship.x - spacing, this.ship.y+this.ship.h/2);
+					const bullet4 = new Spread(this.game, this.ship, this.ship.x + spacing-5, this.ship.y+this.ship.h/2);
+					this.bullets.push(bullet3);
+					this.bullets.push(bullet4);
+
+			}
+			if(this.ship.weaponStr > 2) {
+				let spacing = this.ship.w/4;
+				const bullet5 = new Spread(this.game, this.ship, this.ship.x - spacing);
+				const bullet6 = new Spread(this.game, this.ship, this.ship.x + spacing-5);
+					this.bullets.push(bullet5);
+					this.bullets.push(bullet6);
+			}
+			if(this.ship.weaponStr > 3) {
+				console.log('now the tricky part');
+			}
 		}
 	}
 	
@@ -32,5 +51,4 @@ class BulletFactory {
 		
 	
 	}
-
 }
--- public/js/bullets/Spread.js
+++ public/js/bullets/Spread.js
@@ -1,26 +1,30 @@
 class Spread extends Bullet {
-	constructor(game, ship) {
+	constructor(game, ship, x=0, y=0) {
 		super(game, ship);
 
 		this.class = 'player';
+		this.ship - ship;
 		this.vy = -20;
 		this.vx = 0;
 		this.power = ship.weaponStr * 4;
 		this.w = 5;
 		this.h = 10;
 		// this.x = ship.x - this.w;
-		this.x = ship.x - this.w/2;
-		this.y = ship.y;
+		x !== 0 ? this.x = x : this.x = ship.x - this.w/2;
+		y !== 0 ? this.y = y : this.y = ship.y;
 
 	}
 
 	draw() {
-		this.x += this.vx;
 		this.y += this.vy;
+		this.x += this.vx;
 		this.context.save();
-		this.context.fillStyle = 'red';
 		this.context.translate(this.x, this.y);
-		this.context.fillRect(0, 0, this.w, this.h);
+		this.context.fillStyle = 'purple';
+		this.context.fillRect(0, 0, this.w, this.h)	
 		this.context.restore();
+
 	}
+
+
 }
