dae573530a7fd89d6bbc370af477aa6d10f39ee0
prescottbreeden <prescott.breeden@gmail.com>
2018-09-22 23:03:57 -0700

    quest for a decent bullet sprite continues

--- public/js/bullets/Spread.js
+++ public/js/bullets/Spread.js
@@ -9,10 +9,21 @@ class Spread extends Bullet {
 		this.power = ship.weaponStr * 4;
 		this.w = 5;
 		this.h = 10;
+		this.img = null;
+		this.src = 'public/images/M484BulletCollection2.png';
 		// this.x = ship.x - this.w;
 		x !== 0 ? this.x = x : this.x = ship.x - this.w/2;
 		y !== 0 ? this.y = y : this.y = ship.y;
 
+		this.create();
+
+	}
+
+	create() {
+		if(this.img === null) {
+			this.img = new Image();
+			this.img.src = this.src;
+		}
 	}
 
 	draw() {
@@ -20,8 +31,10 @@ class Spread extends Bullet {
 		this.x += this.vx;
 		this.context.save();
 		this.context.translate(this.x, this.y);
-		this.context.fillStyle = 'purple';
+		this.context.fillStyle = 'red';
 		this.context.fillRect(0, 0, this.w, this.h)	
+		// this.context.drawImage(this.img, 100*this.col, 100*this.row, 100, 100, this.x-50, this.y-50, this.w, this.h);
+		// this.context.drawImage(this.img, 500, 200, 20, 20, 0, 0, 20, 20);
 		this.context.restore();
 
 	}
